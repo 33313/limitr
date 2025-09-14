06a92cab09766cccaec5b9e72e67e805cbcce1ea
33313 <84045642+33313@users.noreply.github.com>
2025-09-15 01:57:59 +0200

    feat: generate & hash keys

---
+++ internal/api/keys.go
@@ -0,0 +1,23 @@
+package api
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/hex"
+)
+
+func HashAPIKey(key string) string {
+	hash := sha256.Sum256([]byte(key))
+	return hex.EncodeToString(hash[:])
+}
+
+func GenerateAPIKey() (string, error) {
+	length := 24
+	bytes := make([]byte, length)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+	key := base64.RawURLEncoding.EncodeToString(bytes)
+	return key, nil
+}
